6c49a744bfe5928b1b20a8272bd1200909bb5135
alpha-hexor <itsalexilliams69@gmail.com>
2023-08-15 06:46:48 -0700

    refreshing...

--- get_domain.py
+++ get_domain.py
@@ -8,7 +8,7 @@ import json
 user_agents = ['Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
                'Mozilla/5.0 (X11; Linux i686; rv:109.0) Gecko/20100101 Firefox/116.0']
 
-main_url = "https://yugen.to"
+main_url = "https://yugenanime.tv"
 client = httpx.Client(
     headers={
         'user-agent' : random.choice(user_agents)
@@ -37,13 +37,16 @@ else:
     main_domain = ""
     api_domain = ""
 
-s = client.get("https://www.google.com")
+zoro_main = "https://aniwatch.to/"
+s=client.get(zoro_main)
+zoro_domain = s.url
 
 data = {
-    "main_domain" : main_domain,
-    "api_domain" : api_domain,
-    "status_code" : str(r.status_code),
-    "google_code" : str(s.status_code)
+    "yugen_main_domain" : main_domain,
+    "yugen_api_domain" : api_domain,
+    "zoror_main_domain" : zoro_domain,
+    "yugen_status_code" : str(r.status_code),
+    "zoro_status_code" : str(s.status_code)
 }
 
 with open("domains.json", "w") as json_file:
